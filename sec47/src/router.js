052686761e773a55b77dc5b194684a63811d4e32
FuminoriArimoto <arimoto@queserser.co.jp>
2019-01-02 14:45:39 +0900

    Sec51 簡単なSPAを実装した。

---
+++ sec47/src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '@/views/Home.vue'
+import Product from '@/views/Product.vue'
+import ProductList from '@/views/ProductList.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        {path: '/', component: Home},
+        {path: '/product', component: ProductList},
+        {path: '/product/:id', component: Product}
+    ]
+})
+
+export default router
+
---
+++ sec47/src/views/Product.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="product">
+    <h1>商品情報</h1>
+        <p>このページはIDが{{ $route.params.id }}の詳細を表示します。</p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Product"
+    }
+</script>
+
+<style scoped>
+
+</style>
